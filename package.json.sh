7a6f2f29295cecdfa1ec12c5b83cdbb33bcb1e2f
Michael Brade <brade@kde.org>
2015-12-31 16:38:11 -0800

    add some infrastructure

---
+++ package.json.sh
@@ -0,0 +1,50 @@
+#!/usr/local/bin/lsc -cj
+
+name: 'pegjs-latex'
+description: 'A simple PEG.js parser for LaTeX'
+version: '0.1.0'
+
+author:
+    'name': 'Michael Brade'
+    'email': 'brade@kde.org'
+
+keywords:
+    'pegjs'
+    'latex'
+    'parser'
+
+
+scripts:
+    build: 'pegjs latex.pegjs latex.js;'
+    test: 'mocha;'
+
+
+devDependencies:
+    'livescript': '1.4.x'
+
+    'browserify': '12.x'
+    'babelify': '7.x'
+
+
+    ### building
+
+    'pegjs': '0.9.x'
+
+
+    ### testing
+
+    'mocha': '2.3.x'
+    'chai': '3.x'
+    'chai-as-promised': '5.x'
+
+
+repository:
+    type: 'git'
+    url: 'git+https://github.com/michael-brade/pegjs-latex.git'
+
+license: 'MIT'
+
+bugs:
+    url: 'https://github.com/michael-brade/pegjs-latex/issues'
+
+homepage: 'https://github.com/michael-brade/pegjs-latex#readme'
