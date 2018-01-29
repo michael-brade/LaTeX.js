3731eb20c83967fc68affab5bc0452d0c5e82268
Michael Brade <brade@kde.org>
2018-01-29 23:13:55 +0100

    mocha 5 update: setup doesn't need to be specified explicitly anymore

--- package.json.sh
+++ package.json.sh
@@ -57,9 +57,9 @@ scripts:
     pgcc:  "google-closure-compiler --compilation_level SIMPLE \
                                     --externs src/externs.js \
                                     --js_output_file docs/js/playground.bundle.pack.js docs/js/playground.bundle.js;"
-    test:  'mocha test/_*.ls test/tests.ls;'
-    iron:  'iron-node node_modules/.bin/_mocha test/_*.ls test/tests.ls;'
-    cover: 'istanbul cover --dir test/coverage _mocha test/_*.ls test/tests.ls;'
+    test:  'mocha test/tests.ls;'
+    iron:  'iron-node node_modules/.bin/_mocha test/tests.ls;'
+    cover: 'istanbul cover --dir test/coverage _mocha test/tests.ls;'
 
 babel:
     plugins:
