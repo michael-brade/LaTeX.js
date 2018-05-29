f7185bfb319af9497322dee1666cf4d3aaf87ad7
Michael Brade <brade@kde.org>
2018-05-29 23:54:59 +0200

    base.js: throttle calls to resize event listeners (resize events can be excessive)

--- src/js/base.js
+++ src/js/base.js
@@ -42,6 +42,52 @@ function positionMarginpars() {
 }
 
 
+
+// don't call resize event handlers too often
+var optimizedResize = (function() {
+    var callbacks = [],
+        running = false;
+
+    // fired on resize event
+    function resize() {
+        if (!running) {
+            running = true;
+
+            if (window.requestAnimationFrame) {
+                window.requestAnimationFrame(runCallbacks);
+            } else {
+                setTimeout(runCallbacks, 66);
+            }
+        }
+    }
+
+    // run the actual callbacks
+    function runCallbacks() {
+        callbacks.forEach(function(callback) { callback(); });
+        running = false;
+    }
+
+    // adds callback to loop
+    function addCallback(callback) {
+        if (callback) {
+            callbacks.push(callback);
+        }
+    }
+
+    return {
+        // public method to add additional callback
+        add: function(callback) {
+            if (!callbacks.length) {
+                window.addEventListener('resize', resize);
+            }
+            addCallback(callback);
+        }
+    }
+}());
+
+
+// setup event listeners
+
 function completed() {
 	document.removeEventListener("DOMContentLoaded", completed);
 	window.removeEventListener("load", positionMarginpars);
@@ -53,7 +99,8 @@ function completed() {
 
     observer.observe(document, { attributes: true, childList: true, characterData: true, subtree: true });
 
-    window.addEventListener('resize', positionMarginpars);
+    // add resize event listener
+    optimizedResize.add(positionMarginpars);
 
     processTheElements();
     positionMarginpars();
