3a3d9a19d9289ffdec347ea2551cdb1b9d042684
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-06-15 19:30:25 +0900

    Add es modules

--- src/latex.component.js
+++ src/latex.component.js
@@ -1,77 +1,73 @@
-(() => {
+import latexjs from "./latex.esm.js";
 
 // path of this script
-let path = document.currentScript.src
+let path = import.meta.url
 
 // define the <latex-js> tag
-customElements.define('latex-js',
-  class extends HTMLElement {
-    constructor() {
-      super()
-
-      // wait for some LaTeX source to appear in the upgrade-case
-      if (!this.textContent) {
-        const observer = new MutationObserver(mutationList => {
-          if (this.textContent) {
-            // no longer need to watch for change
-            // TODO: actually, we could keep watching to support editing!
-            observer.disconnect();
-            this.onContentReady();
-          }
-        })
-        observer.observe(this, {
-          childList: true
-        })
-      }
-
-      this.shadow =  this.attachShadow({mode: 'open'})
+export default class extends HTMLElement {
+  constructor() {
+    super()
+    this.shadow =  this.attachShadow({mode: 'open'})
+    // wait for some LaTeX source to appear in the upgrade-case
+    if (!this.textContent) {
+      const observer = new MutationObserver(mutationList => {
+        if (this.textContent) {
+          // no longer need to watch for change
+          // TODO: actually, we could keep watching to support editing!
+          observer.disconnect();
+          this.onContentReady();
+        }
+      })
+      observer.observe(this, {
+        childList: true
+      })
+    } else {
+      this.onContentReady();
     }
-
-    onContentReady() {
-      // empty DOM
-      while (this.shadow.lastChild) {
-        this.shadow.lastChild.remove()
-      }
-
-      // read options
-      const hyphenate = this.getAttribute("hyphenate") !== "false"
-
-      if (this.hasAttribute("baseURL"))
-        path = this.getAttribute("baseURL")
-
-      // parse
-      const generator = latexjs.parse(this.textContent, { generator: new latexjs.HtmlGenerator({ hyphenate: hyphenate }) })
-
-      // create DOM
-      let page = document.createElement("div")
-      page.setAttribute("class", "page")
-      page.appendChild(generator.domFragment())
-
-      generator.applyLengthsAndGeometryToDom(this.shadow.host)
-
-      this.shadow.appendChild(generator.stylesAndScripts(path))
-      this.shadow.appendChild(page)
-
-      // we need to add CMU fonts to the parent page (if they weren't added yet)
-      const pDoc = this.ownerDocument
-      const links = pDoc.querySelectorAll('link')
-      const cmu = new URL("fonts/cmu.css", path)
-
-      for (let link of links) {
-        if (link.href == cmu.href)
-          return
-      }
-
-      const linkEl = pDoc.createElement("link")
-      linkEl.type = "text/css"
-      linkEl.rel = "stylesheet"
-      linkEl.href = cmu.href
-
-      pDoc.head.appendChild(linkEl)
-    }
-
-    connectedCallback() { }
   }
-);
 
-})()
+  onContentReady() {
+    // empty DOM
+    while (this.shadow.lastChild) {
+      this.shadow.lastChild.remove()
+    }
+
+    // read options
+    const hyphenate = this.getAttribute("hyphenate") !== "false"
+
+    if (this.hasAttribute("baseURL"))
+      path = this.getAttribute("baseURL")
+
+    // parse
+    const generator = latexjs.parse(this.textContent, { generator: new latexjs.HtmlGenerator({ hyphenate: hyphenate }) })
+
+    // create DOM
+    let page = document.createElement("div")
+    page.setAttribute("class", "page")
+    page.appendChild(generator.domFragment())
+
+    generator.applyLengthsAndGeometryToDom(this.shadow.host)
+
+    this.shadow.appendChild(generator.stylesAndScripts(path))
+    this.shadow.appendChild(page)
+
+    // we need to add CMU fonts to the parent page (if they weren't added yet)
+    const pDoc = this.ownerDocument
+    const links = pDoc.querySelectorAll('link')
+    const cmu = new URL("fonts/cmu.css", path)
+
+    for (let link of links) {
+      if (link.href == cmu.href)
+        return
+    }
+
+    const linkEl = pDoc.createElement("link")
+    linkEl.type = "text/css"
+    linkEl.rel = "stylesheet"
+    linkEl.href = cmu.href
+
+    pDoc.head.appendChild(linkEl)
+  }
+
+  connectedCallback() { }
+}
--- src/latex.ltx.ls
+++ src/latex.ltx.ls
@@ -1186,7 +1186,11 @@ export class LaTeX
         @\documentclass = !-> @g.error "Two \\documentclass commands. The document may only declare one class."
 
         # load and instantiate the documentclass
-        Export = (require "./documentclasses/" + documentclass + ".js")
+        Classes = 
+            \article : require "./documentclasses/article.js"
+            \book    : require "./documentclasses/book.js"
+            \report  : require "./documentclasses/article.js"
+        Export = Classes[documentclass]
         if not Class = Export.default
             Class = Export[Object.getOwnPropertyNames(Export).0]
 
