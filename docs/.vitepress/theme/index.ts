ea42ad6752cf8240b4223c5bf80402f9cb1fc197
Michael Brade <brade@kde.org>
2026-06-15 12:30:16 +0200

    vitepress index.js -> typescript, fix VS Code by adding vite aliases to tsconfig

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,14 @@
+import type { EnhanceAppContext } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+
+import LaTeX from '@theme/components/LaTeX.vue'
+import TeX from '@theme/components/TeX.vue'
+
+export default {
+    extends: DefaultTheme,
+
+    enhanceApp({ app }: EnhanceAppContext) {
+        app.component('LaTeX', LaTeX)
+        app.component('TeX', TeX)
+    }
+}
